7603853c1ade3e9a78aec2e08d5d7f61e624c78e
haesungkim <stevenkim1259@gmail.com>
2026-05-20 16:04:37 +0900

    add initial configs.py

---
+++ configs/configs.py
@@ -0,0 +1,15 @@
+DATA_DIR = "./data"
+BATCH_SIZE = 1  # stochastic
+NUM_WORKERS = 0
+
+LR_SCHEDULE = [  # total 20 passes
+    (2, 0.0005),
+    (3, 0.0002),
+    (3, 0.0001),
+    (4, 0.00005),
+    (8, 0.00001),
+]
+
+MU = 0.02  # step size safety factor
+HESSIAN_SAMPLE_SIZE = 500  # Num of samples for Hessian re-estimation
+J = 0.1  # incorrect class penalty in loss function
